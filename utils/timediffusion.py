fba51ace0c6392bfebade3e379e652988efa3397
Vladislav <hellovlad1999@mail.ru>
2023-01-17 16:42:52 +0800

    model optimization + visualization rescaling

--- utils/timediffusion.py
+++ utils/timediffusion.py
@@ -5,8 +5,6 @@ import numpy as np
 import torch
 from torch import nn
 
-from .dl import QuantGAN_TemporalBlock
-
 def is_high_freq(time_series, threshold=0.5, rolling_parts=200):
     orig_std = time_series.std().values[0]
     ma_ts = time_series.rolling(len(time_series) // rolling_parts).mean()
@@ -24,6 +22,59 @@ def ma(time_series, rolling_parts=200, window=None):
     return ats
 
 
+class Chomp1d(nn.Module):
+    def __init__(self, chomp_size):
+        super(Chomp1d, self).__init__()
+        self.chomp_size = chomp_size
+
+    def forward(self, x):
+        return x[:, :, : - self.chomp_size].contiguous()
+
+
+class QuantGAN_TemporalBlock(nn.Module):
+    def __init__(self, n_inputs, n_outputs, kernel_size, stride, dilation, padding, dropout=0.2):
+        super(QuantGAN_TemporalBlock, self).__init__()
+        self.conv1 = torch.nn.utils.weight_norm(nn.Conv1d(
+            n_inputs, n_outputs, kernel_size, stride=stride, padding=padding, dilation=dilation))
+        self.chomp1 = Chomp1d(padding)
+        self.relu1 = nn.ReLU()
+        if dropout != 0:
+            self.dropout1 = nn.Dropout(dropout)
+
+        self.conv2 = torch.nn.utils.weight_norm(nn.Conv1d(
+            n_outputs, n_outputs, kernel_size, stride=stride, padding=padding, dilation=dilation))
+        self.chomp2 = Chomp1d(padding)
+        self.relu2 = nn.ReLU()
+        if dropout != 0:
+            self.dropout2 = nn.Dropout(dropout)
+
+        if padding == 0:
+            if dropout == 0:
+                self.net = nn.Sequential(self.conv1, self.relu1, self.conv2, self.relu2)
+            else:
+                self.net = nn.Sequential(self.conv1, self.relu1, self.dropout1, self.conv2, self.relu2, self.dropout2)
+        else:
+            if dropout == 0:
+                self.net = nn.Sequential(self.conv1, self.chomp1, self.relu1, self.conv2, self.chomp2, self.relu2)
+            else:
+                self.net = nn.Sequential(self.conv1, self.chomp1, self.relu1, self.dropout1, self.conv2, self.chomp2, self.relu2, self.dropout2)
+
+        self.downsample = nn.Conv1d(n_inputs, n_outputs, 1) if n_inputs != n_outputs else None
+        self.relu = nn.ReLU()
+        self.init_weights()
+
+    def init_weights(self):
+        self.conv1.weight.data.normal_(0, 0.5)
+        self.conv2.weight.data.normal_(0, 0.5)
+        if self.downsample is not None:
+            self.downsample.weight.data.normal_(0, 0.5)
+
+    def forward(self, x):
+        out = self.net(x)
+        res = x if self.downsample is None else self.downsample(x)
+        return out, self.relu(out + res)
+
+
 class TimeDiffusion(nn.Module):
     def __init__(self):
         super().__init__()
@@ -33,11 +84,14 @@ class TimeDiffusion(nn.Module):
         self.last = nn.Conv1d(128, 1, kernel_size=1, stride=1, dilation=1)
 
     def forward(self, x):
-        skip_layers = []
+        skip_acc = None
         for layer in self.tcn:
             skip, x = layer(x)
-            skip_layers.append(skip)
-        x = self.last(x + sum(skip_layers))
+            if skip_acc is None:
+                skip_acc = skip
+            else:
+                skip_acc += skip
+        x = self.last(x + skip_acc)
         return x
 
 def train_forecast(seq: np.array, horizon: int, device, epochs=20, seed=0, lr=4e-4, batch_size=32, steps_per_epoch=32, pbar=False):
@@ -47,18 +101,18 @@ def train_forecast(seq: np.array, horizon: int, device, epochs=20, seed=0, lr=4e
     tmean = seq.mean()
     tstd = seq.std()
     train = (seq - tmean) / tstd
-    train_tensor = torch.from_numpy(train).float().to(device)
+    train_tensor = torch.from_numpy(train).to(device=device, dtype=torch.float32)
 
     torch.random.manual_seed(seed)
-    model = TimeDiffusion().to(device)
+    model = TimeDiffusion().to(device=device, dtype=torch.float32)
     optim = torch.optim.AdamW(model.parameters(), lr=lr)
     losses = []
 
     for epoch in (tqdm(range(1, epochs + 1)) if pbar else range(1, epochs + 1)):
         model.train()
         X = train_tensor.repeat(batch_size, 1).unsqueeze(1)
-        noise = torch.row_stack([torch.rand(1, *X.shape[1:]) for _ in range(X.shape[0])]).to(device)
-        noise_level = torch.rand(X.shape).to(device)
+        noise = torch.row_stack([torch.rand(1, *X.shape[1:]) for _ in range(X.shape[0])]).to(device=device, dtype=torch.float32)
+        noise_level = torch.rand(X.shape).to(device=device, dtype=torch.float32)
         noise *= noise_level
 
         for step in range(steps_per_epoch):
@@ -74,7 +128,7 @@ def train_forecast(seq: np.array, horizon: int, device, epochs=20, seed=0, lr=4e
     model.eval()
     result = []
     with torch.no_grad():
-        generated = torch.rand(1, 1, len(seq) + horizon).to(device)
+        generated = torch.rand(1, 1, len(seq) + horizon).to(device=device, dtype=torch.float32)
         for step in range(1, steps_per_epoch + 1):
             pred_noise = model(generated)
             generated -= pred_noise
